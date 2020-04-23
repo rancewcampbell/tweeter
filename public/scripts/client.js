6635c6572efe665d8ce4f6844fa33e4485a645a4
Rance Campbell <rancewcampbell@gmail.com>
2020-04-23 16:24:14 +0000

    add button to scroll to top of page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -73,6 +73,20 @@ $(document).ready(() => {
     $('.new-tweet textarea').focus();
   });
 
+  $(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 50) {
+        $('#scroll-button').fadeIn(200);
+    } else {
+        $('#scroll-button').fadeOut(200);
+    }
+  });
+
+  $('#scroll-button').click(function(event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+  });
+
   loadTweets();
 
 });
